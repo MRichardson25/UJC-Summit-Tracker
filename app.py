d56e69bf8c9ad919dda4cf0e6d0cc36fa9363e52
Micah Richardson <69762710+MicahRichardson00@users.noreply.github.com>
2025-05-19 11:16:41 -0400

    519 update

--- app.py
+++ app.py
@@ -492,15 +492,15 @@ with tab1:
     # ADVISOR INVITE COUNT 
     num_adv_registered = filtered_advisor_counts["Count"].sum()
     # EVENTBRITE VIEW COUNT (MANUAL)
-    eventbrite_views = 16006
-    previous_eventbrite_views = 14073
+    eventbrite_views = 17094
+    previous_eventbrite_views = 16006
     #test change
     #df1_res, df2_res, df3_res, df4_res = process_and_calc_returners(data_2022,data_2023,survey_data)
     load_dotenv() 
     data_2022 = os.getenv("DATA_2022")
     data_2023 = os.getenv("DATA_2023")
     #df1_res, df4_res, df2_res, df3_res = process_and_calc_returners(data_2022,data_2023,survey_data)
-    df1_res, df4_res, df2_res, df3_res = 690,277,988,198
+    df1_res, df4_res, df2_res, df3_res = 690,281,998,200
     df1_old, df4_old, df2_old, df3_old = 690,276,969,197
 
     # PREVIOUS ATTENDEE COUNT
